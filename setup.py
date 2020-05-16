125f5572251f733348e3b9d2a4a749fe9ce2c7d9
Dennis Cheng <chifung.cheng@gmail.com>
2020-05-16 11:59:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="openapi-schema-pydantic",
     version="0.1.0",
-    author="Dennis Cheng",
-    author_email="chifung.cheng@gmail.com",
+    author="Kuimono",
     description="OpenAPI (v3) specification schema as pydantic class",
     long_description=long_description,
     long_description_content_type="text/markdown",
