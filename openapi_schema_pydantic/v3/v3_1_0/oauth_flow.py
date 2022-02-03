65eec31c5d23210b06ca52149ba4b4c12c4bde1d
Dennis Cheng <chifung.cheng@gmail.com>
2022-02-03 15:56:22 +0800

    Fix issue #5 by allowing str for url types in `SecurityScheme`, `OAuthFlow`

--- openapi_schema_pydantic/v3/v3_1_0/oauth_flow.py
+++ openapi_schema_pydantic/v3/v3_1_0/oauth_flow.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 
 from pydantic import AnyUrl, BaseModel, Extra
 
@@ -8,7 +8,7 @@ class OAuthFlow(BaseModel):
     Configuration details for a supported OAuth Flow
     """
 
-    authorizationUrl: Optional[AnyUrl] = None
+    authorizationUrl: Optional[Union[AnyUrl, str]] = None
     """
     **REQUIRED** for `oauth2 ("implicit", "authorizationCode")`.
     The authorization URL to be used for this flow.
@@ -16,7 +16,7 @@ class OAuthFlow(BaseModel):
     The OAuth2 standard requires the use of TLS.
     """
 
-    tokenUrl: Optional[AnyUrl] = None
+    tokenUrl: Optional[Union[AnyUrl, str]] = None
     """
     **REQUIRED** for `oauth2 ("password", "clientCredentials", "authorizationCode")`.
     The token URL to be used for this flow.
@@ -24,7 +24,7 @@ class OAuthFlow(BaseModel):
     The OAuth2 standard requires the use of TLS.
     """
 
-    refreshUrl: Optional[AnyUrl] = None
+    refreshUrl: Optional[Union[AnyUrl, str]] = None
     """
     The URL to be used for obtaining refresh tokens.
     This MUST be in the form of a URL.
@@ -49,6 +49,12 @@ class OAuthFlow(BaseModel):
                 {
                     "authorizationUrl": "https://example.com/api/oauth/dialog",
                     "tokenUrl": "https://example.com/api/oauth/token",
+                    "scopes": {"write:pets": "modify pets in your account", "read:pets": "read your pets"},
+                },
+                {
+                    "authorizationUrl": "/api/oauth/dialog",  # issue #5: allow relative path
+                    "tokenUrl": "/api/oauth/token",  # issue #5: allow relative path
+                    "refreshUrl": "/api/oauth/token",  # issue #5: allow relative path
                     "scopes": {"write:pets": "modify pets in your account", "read:pets": "read your pets"},
                 },
             ]
